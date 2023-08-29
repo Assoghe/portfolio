89876d5ff00718328619f87f3a3f60553aaf3a52
Assoghe <soraya.gherras17@gmail.com>
2023-08-29 17:02:52 +0200

    02 > Portfolio > Layout (color)

--- portfolioso/src/Pages/Portfolio.jsx
+++ portfolioso/src/Pages/Portfolio.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import { styled } from 'styled-components'
 
 function Portfolio() {
   return (
-    <>
+    <PortfolioHerosection>
     <div className='bloc-title'>
       <h3> Je suis </h3>
       <h1> UX UI designer et intégrateur web</h1>  
@@ -11,9 +12,21 @@ function Portfolio() {
     <div className='bloc-paragraph'>
       <p> Entre design et développement, je donne vie à des interfaces réalistes. Chaque projet est une occasion d'apprendre, d'innover et de créer des expériences uniques pour les utilisateurs.</p>
     </div>
-    
-    </>
+
+    </PortfolioHerosection>
   )
 }
+
+const PortfolioHerosection = styled.div`
+  background-color: red;
+
+  .bloc-title {
+    background-color: blue;
+  }
+
+  .bloc-paragraph {
+    background-color: green;
+  }
+`
 
 export default Portfolio
