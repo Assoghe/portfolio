b22fb33608bee26fa78babf6c1ec92521a41399a
Assoghe <soraya.gherras17@gmail.com>
2023-08-29 15:36:58 +0200

    Navigation > Menu > Add Mulish font and style Menu

--- portfolioso/src/Components/Menu.jsx
+++ portfolioso/src/Components/Menu.jsx
@@ -35,10 +35,11 @@ const NavigationStyled = styled.div `
   color: #D9D9D9;
   background: #1A1C23;
   padding: 0 10%;
+  position: sticky;
 
   h3 {
 font-family: 'Philosopher', sans-serif; 
-font-size: 24px;
+font-size: 1.5em;
 font-style: normal;
 font-weight: 600;
 line-height: normal;
@@ -52,6 +53,29 @@ letter-spacing: 0.96px;
   display: flex;
   }
 
+  .navigation-right a, svg {
+    margin-right: 1.5em;
+    text-decoration: none;
+    color: #D9D9D9;
+  }
+
+  .navigation-right a {
+    font-family: Mulish, sans-serif;
+font-size: 1.2em;
+font-style: normal;
+font-weight: 400;
+line-height: 2.25rem; /* 150% */
+  }
+
+  .navigation-right svg {
+    font-size: 1.2em;
+    align-item: center;
+  }
+
+  .socialnetworks {
+    align-items: center;
+    display: flex;
+  }
 `
 
 export default Menu
