5436807629849b5e8375b09e7a4a749fdc6c00a9
Assoghe <soraya.gherras17@gmail.com>
2023-08-29 16:59:49 +0200

    02 > Portfolio > Add layout

--- portfolioso/src/Pages/Portfolio.jsx
+++ portfolioso/src/Pages/Portfolio.jsx
@@ -2,9 +2,17 @@ import React from 'react'
 
 function Portfolio() {
   return (
-    <div>
-      Portfolio
+    <>
+    <div className='bloc-title'>
+      <h3> Je suis </h3>
+      <h1> UX UI designer et intégrateur web</h1>  
     </div>
+
+    <div className='bloc-paragraph'>
+      <p> Entre design et développement, je donne vie à des interfaces réalistes. Chaque projet est une occasion d'apprendre, d'innover et de créer des expériences uniques pour les utilisateurs.</p>
+    </div>
+    
+    </>
   )
 }
 
