49ebabf9d96029fde625654cd02324a32f534018
Assoghe <soraya.gherras17@gmail.com>
2023-08-29 13:29:01 +0200

    Navigation > Menu > Layout link

--- portfolioso/src/Components/Menu.jsx
+++ portfolioso/src/Components/Menu.jsx
@@ -1,18 +1,21 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { styled } from 'styled-components'
 
 function Navigation() {
   return (
-    <>
-    <nav>
+    <NavigationStyled>
     <Link to='/'> Portfolio </Link>
       <Link to='/about'> A propos </Link>
       <Link to='/contact'> Contactez-moi ! </Link>
-    </nav>
-
-    
-    </>
+    </NavigationStyled>
   )
 }
+
+const NavigationStyled = styled.div `
+  width: 100%;
+  padding: 1em 0;
+  background-color: red;
+`
 
 export default Navigation
