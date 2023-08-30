99e283fb6dd4060bd6d56b5a6d186512a7fc97ee
Assoghe <soraya.gherras17@gmail.com>
2023-08-30 15:12:06 +0200

    02 Portfolio > Portfolio > Add style

--- portfolioso/src/Pages/Portfolio.jsx
+++ portfolioso/src/Pages/Portfolio.jsx
@@ -3,8 +3,9 @@ import { styled } from 'styled-components'
 
 function Portfolio() {
   return (
-    <PortfolioHerosection>
-    <div className='bloc-title'>
+  <PortfolioHerosection>
+  <div className='bloc-hero'>  
+    <div className='bloc-title'> 
       <h3> Je suis </h3>
       <h1> UX UI designer et intégrateur web</h1>  
     </div>
@@ -12,21 +13,44 @@ function Portfolio() {
     <div className='bloc-paragraph'>
       <p> Entre design et développement, je donne vie à des interfaces réalistes. Chaque projet est une occasion d'apprendre, d'innover et de créer des expériences uniques pour les utilisateurs.</p>
     </div>
-
-    </PortfolioHerosection>
+  </div>  
+  </PortfolioHerosection>
   )
 }
 
-const PortfolioHerosection = styled.div`
-  background-color: red;
 
-  .bloc-title {
-    background-color: blue;
+const PortfolioHerosection = styled.div`
+  height: 80vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center; 
+  margin-right: 10%;
+  margin-left: 10%;
+
+  .bloc-hero {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+  }
+
+  .bloc-title,
+  .bloc-paragraph {
+    padding: 1em;
+  }
+
+
+  .bloc-title h1 {
+    margin: 0;
   }
 
   .bloc-paragraph {
-    background-color: green;
+    align-self: flex-end;
   }
-`
 
-export default Portfolio
+  .bloc-paragraph p {
+    margin: 0;
+  }
+`;
+
+export default Portfolio;
+
